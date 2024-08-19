a297681a780f28feb44e164c98f05716abf9b792
Kevin Liwag <kevin@iship.com>
2024-08-18 20:14:06 -0700

    syncing work settings; adding VSCode folding bindings

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ vim.o.clipboard = "unnamedplus"
 -- Set number of lines to show above and below the cursor
 vim.o.scrolloff = 8
 
+-- Set to use tabs instead of spaces
+vim.opt.expandtab = false
+
 -- Show line numbers
 vim.wo.number = true
 
@@ -14,39 +17,74 @@ vim.o.smartcase = true
 -- Set leader key to <space>
 vim.g.mapleader = " "
 
--- Add Lazy.Nvim package manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Load Lazy.nvim 
-require('lazy').setup({
-  -- undotree plugin
-  {
-    'mbbill/undotree',
-    config = function()
-      -- Optionally set up any configuration you need
-      vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true, silent = true })
+-- Enable relative line numbers in NORMAL mode
+vim.api.nvim_create_autocmd("InsertEnter", {
+    pattern = "*",
+    callback = function()
+        vim.wo.relativenumber = false
     end,
-  },
-  -- More plugins
 })
 
--- Load vim-plug
-vim.cmd [[
-call plug#begin('~/.local/share/nvim/plugged')
+vim.api.nvim_create_autocmd("InsertLeave", {
+    pattern = "*",
+    callback = function()
+        vim.wo.relativenumber = true
+    end,
+})
 
-" GitHub Copilot plugin
-Plug 'github/copilot.vim'
+-- Ensure relative line numbers are enabled initially
+vim.wo.relativenumber = true
 
-call plug#end()
-]]
+-- Begin vim-plug setup
+vim.cmd([[
+  call plug#begin('~/.config/nvim/plugged')
+
+  Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+  Plug 'junegunn/fzf.vim'
+  Plug 'nvim-lua/plenary.nvim'
+  Plug 'ThePrimeagen/harpoon'
+  call plug#end() 
+]])
+
+-- Setup VSCode commands
+-- Map Neovim folding commands to VSCode folding commands
+-- Check if running in VSCode with the Neovim extension
+-- NOTE: Macros do not work with this solution
+if vim.g.vscode then
+	-- fzf using extension
+	vim.cmd([[
+	command! -nargs=0 F call VSCodeNotify('fzf-quick-open.runFzfFileProjectRoot')
+	]], false)
+
+	-- add :f abbreviation
+	vim.cmd('cnoreabbrev f F')
+
+	-- Map Neovim folding commands to VSCode folding commands
+	vim.api.nvim_set_keymap('n', 'zM', ':call VSCodeNotify("editor.foldAll")<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', 'zR', ':call VSCodeNotify("editor.unfoldAll")<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', 'zc', ':call VSCodeNotify("editor.fold")<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', 'zC', ':call VSCodeNotify("editor.foldRecursively")<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', 'zo', ':call VSCodeNotify("editor.unfold")<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', 'zO', ':call VSCodeNotify("editor.unfoldRecursively")<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', 'za', ':call VSCodeNotify("editor.toggleFold")<CR>', { noremap = true, silent = true })
+
+	-- Function to move the cursor without unfolding
+	function MoveCursor(direction)
+		local result
+		if vim.v.count == 0 and vim.fn.reg_recording() == '' and vim.fn.reg_executing() == '' then
+			result = 'g' .. direction
+		else
+			result = direction
+		end
+		--print("MoveCursor result: " .. result)
+		return result
+	end
+
+	-- Map j and k to move cursor and avoid unfolding
+	vim.keymap.set('n', 'k', function()
+		return MoveCursor('k')
+	end, { noremap = true, expr = true, silent = true })
+	vim.keymap.set('n', 'j', function()
+		return MoveCursor('j')
+	end, { noremap = true, expr = true, silent = true})
+end
