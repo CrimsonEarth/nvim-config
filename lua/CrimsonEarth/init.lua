f53b887510dad9175b13f03e53a250a5c37f89fd
Kevin Liwag <kevin@iship.com>
2024-10-22 11:56:31 -0700

    adding marks and registers keymappings

--- lua/CrimsonEarth/init.lua
+++ lua/CrimsonEarth/init.lua
@@ -7,3 +7,25 @@ if not is_vscode then
     require("CrimsonEarth.lazy_init");
 end
 
+-- Windows Configuration for NetRW
+if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+  -- Configure copy command
+  vim.g.netrw_localcopycmd = 'cmd.exe'
+  vim.g.netrw_localcopyopt = '/c copy /y'
+
+  -- Configure move command
+  vim.g.netrw_localmovecmd = 'cmd.exe'
+  vim.g.netrw_localmoveopt = '/c move /y'
+
+  -- Configure delete command
+  vim.g.netrw_localrmcmd = 'cmd.exe'
+  vim.g.netrw_localrmopt = '/c del /f /q'
+
+  -- Configure mkdir command
+  vim.g.netrw_localmkdir = 'cmd.exe'
+  vim.g.netrw_localmkdiropt = '/c mkdir'
+
+  -- Configure rmdir command
+  vim.g.netrw_localrmdir = 'cmd.exe'
+  vim.g.netrw_localrmdiropt = '/c rmdir /s /q'
+end
--- lua/CrimsonEarth/remap.lua
+++ lua/CrimsonEarth/remap.lua
@@ -6,9 +6,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- "adjustments"
 
 -- Marks
+vim.keymap.set('n', '<leader>m', ':marks<CR>', { noremap = true, silent = true } )
 -- only show the UserDefined Marks (a-zA-Z)
-vim.api.nvim_create_user_command('UserMarks', function()
-  local marks = ''
+function UserMarks()
+    local marks = ''
   for i = string.byte('a'), string.byte('z') do
     marks = marks .. string.char(i)
   end
@@ -16,4 +17,29 @@ vim.api.nvim_create_user_command('UserMarks', function()
     marks = marks .. string.char(i)
   end
   vim.cmd('marks ' .. marks)
-end, {})
+end
+
+vim.keymap.set('n', '<leader>my', function()
+    UserMarks()
+end, { noremap = true, silent = true })
+
+-- Registers
+vim.keymap.set('n', '<leader>r', ':registers<CR>', { noremap = true, silent = true })
+
+-- only show the User Defined Registers (a-zA-Z)
+function UserRegisters()
+    local regs = ''
+  for i = string.byte('a'), string.byte('z') do
+    regs = regs .. string.char(i)
+  end
+  for i = string.byte('A'), string.byte('Z') do
+    regs = regs .. string.char(i)
+  end
+  vim.cmd('registers ' .. regs)
+end
+
+vim.keymap.set('n', '<leader>ry', function()
+    UserRegisters()
+end, { noremap = true, silent = true })
+
+
